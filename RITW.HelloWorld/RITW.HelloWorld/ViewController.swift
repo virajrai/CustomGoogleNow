d03d10b8b3822d03721d36e179590923dd186478
Zhang Xindong <zxdxjtu@stu.xjtu.edu.cn>
2017-02-07 16:55:50 -0500

    hello world for veraj

---
+++ RITW.HelloWorld/RITW.HelloWorld/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  RITW.HelloWorld
+//
+//  Created by XIndong Zhang on 2017/1/28.
+//  Copyright © 2017年 SmackInnovations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func retrieveData(_ sender: Any) {
+        let userDefaults = UserDefaults(suiteName: "group.com.RITW")
+        if let testUserId = userDefaults?.object(forKey: "userId") as? String {
+            print("User Id: \(testUserId)")
+        }
+        
+    }
+
+}
+
